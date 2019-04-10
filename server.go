d0b10e8bc5f12eac162d98dbc4c0a7878f35019d
Erik Campobadal <soc@erik.cat>
2019-04-10 15:46:38 +0200

    Config splitted

--- server.go
+++ server.go
@@ -5,12 +5,11 @@ import (
 
 	"github.com/pulsar-go/example/routes"
 	"github.com/pulsar-go/pulsar"
-	"github.com/pulsar-go/pulsar/config"
 )
 
 func main() {
 	// Get the settings from the configuration files.
-	config.Set("./server.toml")
+	// config.Set("./server.toml")
 	// Set the application routes.
 	routes.Register()
 	// Serve the HTTP server.
